809b3a568b4460806c574173a202c7bcb579e152
unknown <aswiniachu388@gmail.com>
2025-03-18 14:56:17 +0530

    (added)

---
+++ ASP.NetCore/Chapter 4/aswini asses collection/COLLECTIONSASSESMEN/COLLECTIONSASSESMEN/SHOPPINCART.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COLLECTIONSASSESMEN
+{
+    public class SHOPPINCART
+    {
+         List<ITE_M> items = new List<ITE_M>();
+        public void AddItem(ITE_M item)
+        {
+            items.Add(item);
+        }
+        public void RemoveItem(string itemname)
+        {
+            ITE_M itemtoremove =items.RemoveItem
+        }
+
+        public double CalculateTotalPrice()
+        {
+
+            return items.Sum(items.price * items.quantity);
+        }
+    }
+}
